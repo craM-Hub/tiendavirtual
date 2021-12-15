87b90a33e77d3e1dba25f3d7daa237979fbab10c
craM-Hub <anarka84@gmail.com>
2021-12-15 10:33:29 +0100

    destacados done, sigue 1.3 novedades

--- src/app/controllers/PageController.php
+++ src/app/controllers/PageController.php
@@ -1,6 +1,8 @@
 <?php
+
 namespace ProyectoWeb\app\controllers;
 
+use ProyectoWeb\repository\ProductRepository;
 use Psr\Container\ContainerInterface;
 
 
@@ -9,11 +11,20 @@ class PageController
     protected $container;
 
     // constructor receives container instance
-    public function __construct(ContainerInterface $container) {
+    public function __construct(ContainerInterface $container)
+    {
         $this->container = $container;
     }
-    public function home($request, $response, $args) {
+    public function home($request, $response, $args)
+    {
         $title = "Inicio";
-        return $this->container->renderer->render($response, "index.view.php", compact('title'));
+        $repositorio = new ProductRepository();
+        $carrusel = $repositorio->getCarrusel();
+        $destacados = $repositorio->getDestacados();
+        return $this->container->renderer->render(
+            $response,
+            "index.view.php",
+            compact('title', 'carrusel', 'destacados')
+        );
     }
 }
--- src/repository/ProductRepository.php
+++ src/repository/ProductRepository.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace ProyectoWeb\repository;
 
 use ProyectoWeb\database\QueryBuilder;
@@ -6,7 +7,20 @@ use ProyectoWeb\database\QueryBuilder;
 
 class ProductRepository extends QueryBuilder
 {
-    public function __construct(){
+    public function __construct()
+    {
         parent::__construct('productos', 'Product');
+    }
+
+    public function getCarrusel()
+    {
+        $sql = "SELECT * FROM $this->table WHERE carrusel IS NOT NULL AND carrusel != ''";
+        return $this->executeQuery($sql);
+    }
+
+    public function getDestacados()
+    {
+        $sql = "SELECT * FROM $this->table WHERE destacado = 1";
+        return $this->executeQuery($sql);
     }
 }
